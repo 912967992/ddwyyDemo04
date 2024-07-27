91eb00874cdcc1d5fce38a164f781d45ec79c109
912967992 <68109588+912967992@users.noreply.github.com>
2024-07-27 14:21:32 +0800

    Delete src directory

--- src/main/java/com/lu/ddwyydemo04/exceptions/SessionTimeoutException.java
+++
@@ -1,7 +0,0 @@
-package com.lu.ddwyydemo04.exceptions;
-
-public class SessionTimeoutException extends RuntimeException {
-    public SessionTimeoutException(String message) {
-        super(message);
-    }
-}
