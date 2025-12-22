f9d2eab1ce13669b837342eb1276bcc875679e7e
912967992 <912967992@qq.com>
2025-12-22 18:02:54 +0800

    20251222提交报告的时候新增对zip的压缩比提高处理

--- src/main/java/com/lu/ddwyydemo04/DdwyyDemo04Application.java
+++ src/main/java/com/lu/ddwyydemo04/DdwyyDemo04Application.java
@@ -1,5 +1,6 @@
 package com.lu.ddwyydemo04;
 
+import org.apache.poi.openxml4j.util.ZipSecureFile;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,6 +20,12 @@ public class DdwyyDemo04Application {
         System.setProperty("sun.net.client.defaultConnectTimeout", "30000");
         // 读取超时：30秒
         System.setProperty("sun.net.client.defaultReadTimeout", "30000");
+        
+        // 配置 Apache POI ZipSecureFile 以允许处理包含嵌入 OLE 对象的 Excel 文件
+        // 默认的 MIN_INFLATE_RATIO 是 0.01，当压缩比等于或低于此值时会被拒绝
+        // 设置为 0.001 以允许更低的压缩比（例如包含大量图片的 Excel 文件）
+        // 注意：这会降低对 Zip bomb 攻击的防护，但允许处理正常的业务文件
+        ZipSecureFile.setMinInflateRatio(0.001);
         
         SpringApplication.run(DdwyyDemo04Application.class, args);
     }
